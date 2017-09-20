ce08c5d96e1c89fa485264ed3dbdfe940ee79759
Ryan Romano <ryanaromano@gmail.com>
2017-09-20 14:38:26 -0400

    working hotloader

---
+++ src/theme/globalStyle.js
@@ -0,0 +1,13 @@
+import { injectGlobal } from 'styled-components';
+
+
+injectGlobal`
+	@import url('https://fonts.googleapis.com/css?family=VT323');
+
+	body {
+		font-family: 'VT323', monospace;
+		font-size: 24px;
+		margin: 0;
+  		padding: 0;
+	}
+`
